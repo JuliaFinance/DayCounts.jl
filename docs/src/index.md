5cf1649326fa6d4bd580a293bac7f20ba817eda2
Simon Byrne <simonbyrne@gmail.com>
2019-01-18 12:08:29 -0800

    more tests, and add docs

---
+++ docs/src/index.md
@@ -0,0 +1,61 @@
+# DayCounts.jl
+
+DayCounts.jl provides calculations of various day count conventions used in finance for
+computing accrued interest and discount factors. These conventions have arisen to handle
+factors such as months of different lengths and leap days.
+
+# Example
+
+```jldoctest
+julia> using DayCounts, Dates
+
+julia> d1, d2 = Date("2019-01-01"), Date("2019-04-01") # standard year
+(2019-01-01, 2019-04-01)
+
+julia> yearfraction(d1, d2, Thirty360())
+0.25
+
+julia> yearfraction(d1, d2, Actual360())
+0.25
+
+julia> yearfraction(d1, d2, Actual365Fixed())
+0.2465753424657534
+
+julia> yearfraction(d1, d2, ActualActualISDA())
+0.2465753424657534
+
+julia> d1, d2 = Date("2020-01-01"), Date("2020-04-01") # standard year
+(2020-01-01, 2020-04-01)
+
+julia> yearfraction(d1, d2, Thirty360())
+0.25
+
+julia> yearfraction(d1, d2, Actual360())
+0.25277777777777777
+
+julia> yearfraction(d1, d2, Actual365Fixed())
+0.2493150684931507
+
+julia> yearfraction(d1, d2, ActualActualISDA())
+0.24863387978142076
+```
+
+# External Links
+- [_Day count convention_ on Wikipedia](https://en.wikipedia.org/wiki/Day_count_convention)
+- [Nasdaq Day Count Fractions](https://business.nasdaq.com/media/day-count-fractions_tcm5044-53854.pdf)
+
+# Interface
+```@docs
+yearfraction
+```
+
+# [`DayCount` types](@id daycount_types)
+```@docs
+Actual365Fixed
+Actual360
+ActualActualISDA
+Thirty360
+ThirtyE360
+ThirtyE360ISDA
+```
+
--- src/DayCounts.jl
+++ src/DayCounts.jl
@@ -5,14 +5,15 @@ module DayCounts
 
 using Dates
 
-export yearfraction, DayCount, Thirty360, ThirtyE360, ThirtyE360ISDA, Actual360, Actual365Fixed, ActualActualISDA
-
+export yearfraction, DayCount,
+    Actual365Fixed, Actual365F, Actual360, ActualActualISDA,
+    Thirty360, BondBasis, ThirtyE360, EurobondBasis, ThirtyE360ISDA
 
 """
     yearfraction(startdate::Date, enddate::Date, dc::DayCount)
 
 Compute the fractional number of years between `startdate` and `enddate`, according to the
-day count convention `dc`.
+[`DayCount` object](@ref daycount_types) `dc`.
 """
 function yearfraction
 end
@@ -24,9 +25,12 @@ abstract type DayCount end
     Actual365Fixed()
     Actual365F()
 
-"Actual/365 (Fixed)" day count convention.
+**Actual/365 (Fixed)** day count convention.
 
-The actual number of days divided by 365.
+The year fraction is computed as:
+```math
+\\frac{\\text{# of days}}{365}
+```
 
 # Reference
  - 2006 ISDA definitions, §4.16 (d)
@@ -40,9 +44,12 @@ end
 """
     Actual360()
 
-"Actual/360" day count convention.
+**Actual/360** day count convention.
 
-Actual number of days divided by 360.
+The year fraction is computed as:
+```math
+\\frac{\\text{# of days}}{360}
+```
 
 # Reference
  - 2006 ISDA definitions, §4.16 (e)
@@ -57,11 +64,15 @@ end
 """
     ActualActualISDA()
 
-"Actual/Actual (ISDA)" day count convention.
+**Actual/Actual (ISDA)** day count convention.
 
-The actual number of days which fall in a standard year divided by 365 plus the actual
-number of days which fall in a leap year divided by 365. The start date is included, and
-the end date is excluded.
+The year fraction is computed as:
+```math
+\\frac{\\text{# of days in standard year}}{365} +
+\\frac{\\text{# of days in leap year}}{366}
+```
+
+For the purposes of above, the start date is included and the end date is excluded.
 
 # Reference
  - 2006 ISDA definitions, §4.16 (b)
@@ -88,7 +99,19 @@ thirty360(dy,dm,dd) = (360*dy + 30*dm + dd)/360
     Thirty360()
     BondBasis()
 
-"30/360" or "Bond Basis" day count convention.
+**30/360** or **Bond Basis** day count convention.
+
+The year fraction is computed as:
+```math
+\\frac{360 \\times (y_2 - y_1) + 30 \\times (m_2 - m_1) + (d_2 - d_1)}{360}
+```
+where
+- ``y_1`` and ``y_2`` are the years of the start and end date, respectively.
+- ``m_1`` and ``m_2`` are the months of the start and end date, respectively.
+- ``d_1`` is the day of the month at the start date, unless it is 31, in which case it is
+  30.
+- ``d_2`` is the day of the month at the end date, unless it is 31 and ``d_1 > 29``, in
+  which case it is 30.
 
 # Reference
  - 2006 ISDA definitions, §4.16 (f)
@@ -98,12 +121,12 @@ const BondBasis = Thirty360
 function yearfraction(startdate::Date, enddate::Date, ::Thirty360)
     dy = year(enddate)-year(startdate)
     dm = month(enddate)-month(startdate)
-    
+
     d1 = day(startdate)
     d2 = day(enddate)
     if d1 >= 30
-        d2 = min(d2,30)
         d1 = 30
+        d2 = min(d2,30)
     end
     return thirty360(dy,dm,d2-d1)
 end
@@ -111,8 +134,20 @@ end
 """
     ThirtyE360()
     EurobondBasis()
-    
-"30E/360" or "Eurobond Basis" day count convention.
+
+**30E/360** or **Eurobond Basis** day count convention.
+
+The year fraction is computed as:
+```math
+\\frac{360 \\times (y_2 - y_1) + 30 \\times (m_2 - m_1) + (d_2 - d_1)}{360}
+```
+where
+- ``y_1`` and ``y_2`` are the years of the start and end date, respectively.
+- ``m_1`` and ``m_2`` are the months of the start and end date, respectively.
+- ``d_1`` is the day of the month at the start date, unless it is 31, in which case it is
+  30.
+- ``d_2`` is the day of the month at the end date, unless it is 31, in which case it is
+  30.
 
 # Reference
  - 2006 ISDA definitions, §4.16 (g)
@@ -129,8 +164,20 @@ end
 
 """
     ThirtyE360ISDA(maturity::Date)
-    
-"30E/360 (ISDA)" day count convention.
+
+**30E/360 (ISDA)** day count convention.
+
+The year fraction is computed as:
+```math
+\\frac{360 \\times (y_2 - y_1) + 30 \\times (m_2 - m_1) + (d_2 - d_1)}{360}
+```
+where
+- ``y_1`` and ``y_2`` are the years of the start and end date, respectively.
+- ``m_1`` and ``m_2`` are the months of the start and end date, respectively.
+- ``d_1`` is the day of the month at the start date, unless it is the last day of
+  February, or 31, in which case it is 30.
+- ``d_2`` is the day of the month at the end date, unless it is the last day of February
+  and not the maturity date, or 31, in which case it is 30.
 
 # Reference
  - 2006 ISDA definitions, §4.16 (h)
@@ -138,15 +185,14 @@ end
 struct ThirtyE360ISDA <: DayCount
     maturity::Date
 end
-function yearfrac(startdate::Date, enddate::Date, dc::ThirtyE360ISDA)
+function yearfraction(startdate::Date, enddate::Date, dc::ThirtyE360ISDA)
     y1 = year(startdate)
     y2 = year(enddate)
-    dm = month(enddate)-month(startdate)
-    d1 = day(startdate)
-    d2 = day(enddate)
-    d1 = d1 == lastdayofmonth(Date(y1,2)) ? 30 : min(d1,30)
-    d2 = ((d2 == lastdayofmonth(Date(y1,2))) & (enddate != dc.maturity)) ? 30 : min(d1,30)
-    return thirty360(y2-y1,dm,d2-d1)
+    m1 = month(startdate)
+    m2 = month(enddate)
+    d1 = startdate == lastdayofmonth(startdate) ? 30 : day(startdate)
+    d2 = enddate == lastdayofmonth(enddate) && !(m2 == 2 && enddate == dc.maturity) ? 30 : day(enddate)
+    return thirty360(y2-y1,m2-m1,d2-d1)
 end
 
 end # module
