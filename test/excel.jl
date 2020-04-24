e83ff59a0aebd546202f8e8bee26845db8bbb7c7
Alec Loudenback <alecloudenback@gmail.com>
2020-04-23 21:42:34 -0500

    switch dependency from ExcelFiles to XLSX
    tests were breaking because ExcelFiles had a Python dependency that was broken.
    
    Also made separate test environment to keep primary Project dependencies light

--- test/excel.jl
+++ test/excel.jl
@@ -1,41 +1,49 @@
-using Test, DayCounts, Dates, ExcelFiles
+using Test, DayCounts, Dates
+using XLSX
+using Tables
+
+xf = XLSX.readxlsx("yearfrac.xlsx")
+
+function range_to_table(rng)
+    Tables.table(rng[2:end,:]; header = Symbol.(rng[1,:]))
+end
 
 @testset "Excel basis 0" begin
-    table = load("yearfrac.xlsx","basis0")
+    table = range_to_table(xf["basis0"][:])
     dc = DayCounts.Thirty360Excel()
-    for r in ExcelFiles.getiterator(table)
+    for r in Tables.rows(table)
         @test yearfrac(Date(r.startdate), Date(r.enddate), dc) == r.yearfrac
     end
 end
 
 @testset "Excel basis 1" begin
-    table = load("yearfrac.xlsx","basis1")
+    table = range_to_table(xf["basis1"][:])
     dc = DayCounts.ActualActualExcel()
-    for r in ExcelFiles.getiterator(table)
+    for r in Tables.rows(table)
         @test yearfrac(Date(r.startdate), Date(r.enddate), dc) == r.yearfrac
     end
 end
 
 @testset "Excel basis 2" begin
-    table = load("yearfrac.xlsx","basis2")
+    table = range_to_table(xf["basis2"][:])
     dc = DayCounts.Actual360()
-    for r in ExcelFiles.getiterator(table)
+    for r in Tables.rows(table)
         @test yearfrac(Date(r.startdate), Date(r.enddate), dc) == r.yearfrac
     end
 end
 
 @testset "Excel basis 3" begin
-    table = load("yearfrac.xlsx","basis3")
+    table = range_to_table(xf["basis3"][:])
     dc = DayCounts.Actual365Fixed()
-    for r in ExcelFiles.getiterator(table)
+    for r in Tables.rows(table)
         @test yearfrac(Date(r.startdate), Date(r.enddate), dc) == r.yearfrac
     end
 end
 
 @testset "Excel basis 4" begin
-    table = load("yearfrac.xlsx","basis4")
+    table = range_to_table(xf["basis4"][:])
     dc = DayCounts.ThirtyE360()
-    for r in ExcelFiles.getiterator(table)
+    for r in Tables.rows(table)
         @test yearfrac(Date(r.startdate), Date(r.enddate), dc) == r.yearfrac
     end
 end
